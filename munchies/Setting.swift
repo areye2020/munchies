29559e9ec25daa20b67018bd04a1dfeb8ea81d64
kkirkl <kadkirk@gmail.com>
2026-07-15 16:06:38 -0500

    made Setting class sortable

--- munchies/Setting.swift
+++ munchies/Setting.swift
@@ -5,7 +5,7 @@
 //  Created by Kirkland, Kaden E on 7/15/26.
 //
 
-class Setting: Codable
+class Setting: Codable, Comparable
 {
     let title:String
     var switchState:Bool?
@@ -14,5 +14,15 @@ class Setting: Codable
     {
         self.title = title
         self.switchState = switchState
+    }
+    
+    static func < (lhs:Setting, rhs:Setting) -> Bool
+    {
+        return lhs.title < rhs.title
+    }
+    
+    static func == (lhs:Setting, rhs:Setting) -> Bool
+    {
+        return lhs.title == rhs.title
     }
 }
