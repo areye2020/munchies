aad2e82170ca4e1df6dcc3cf8b1a4e9673ef824a
Logan Nhem <logan.nhem@gmail.com>
2026-07-17 16:56:19 -0500

    login/signup ready before firebase integration

--- munchies/SignupViewController.swift
+++ munchies/SignupViewController.swift
@@ -1,31 +1,27 @@
 //
-//  LoginViewController.swift
+//  SignupViewController.swift
 //  Project: munchies
 //  Eid: ln8223
 //  Course: CS371L
-//  Created by Nhem, Logan on 7/13/26.
+//  Created by Nhem, Logan on 7/17/26.
 //
 
 import UIKit
 
-// remember to add keyboard
-// we want this to segue to a profile screen for setup
-class LoginViewController: UIViewController {
+class SignupViewController: UIViewController {
 
-    @IBOutlet weak var pwField: RoundedTextField!
     @IBOutlet weak var userField: RoundedTextField!
+    @IBOutlet weak var pwField: RoundedTextField!
+    @IBOutlet weak var confirmField: RoundedTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         userField.placeholder = "Username"
         pwField.placeholder = "Password"
+        confirmField.placeholder = "Confirm Password"
         // Do any additional setup after loading the view.
     }
     
-
-    @IBAction func loginPressed(_ sender: Any) {
-        
+    @IBAction func createPressed(_ sender: Any) {
     }
-    
-
 }
